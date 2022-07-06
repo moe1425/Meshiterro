6a27324db8070aa43517a5247669102dce059982
EC2 Default User <ec2-user@ip-172-31-38-29.ap-northeast-1.compute.internal>
2022-07-06 06:55:55 +0000

    [Finish] 4-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :post_images, dependent: :destroy
   # 1対複数の関係＝ユーザー1つに対して複数の投稿ができ、そのユーザーと投稿したものが紐づいてる。
   # :destroyは「1:Nの1側(ユーザー)が削除されたとき、N側(投稿したもの)を全て削除する」という機能。
