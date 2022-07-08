ad24431856e220c4fcc6b58b190bcf25baed8054
EC2 Default User <ec2-user@ip-172-31-38-29.ap-northeast-1.compute.internal>
2022-07-08 10:04:09 +0000

    [Halfway] ヘッダーとフッターが反映されない

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper,js";
+import "boostrap";
+import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
